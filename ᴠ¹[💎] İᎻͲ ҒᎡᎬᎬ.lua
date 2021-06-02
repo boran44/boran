5d702dd9257fedda712554a7811c5da08bd65451
boran44 <84773003+boran44@users.noreply.github.com>
2021-06-02 16:17:38 +0300

    Add files via upload

---
+++ ᴠ¹[💎] İᎻͲ ҒᎡᎬᎬ.lua
@@ -0,0 +1,83 @@
+
+gg.setVisible(false)
+gg.toast(" İʟʟᴇɢᴀʟ Ꮋᴀᴄᴋ Ͳᴇᴀᴍ")
+gg.setVisible(true)
+gg.setVisible(false)
+gg.alert("────⟦ ◎ İʟʟᴇɢᴀʟ Ꮋᴀᴄᴋ Ͳᴇᴀᴍ ◎ ⟧──── ⧠  Տᴄʀiᴘᴛ : Ᏼʏ ᏀᴀᴍᴇᏦᴏʀᴋᴜᴛ  ⧠  ᗪᴇsiɴɢ Ᏼʏ : Ꮇᴀʜsᴜᴍ Ꮐᴜ̈ᴢᴇʟ ", "⚙️ 𝗦𝗖𝗥𝗶𝗣𝗧", "🛡️ 𝗔𝗡𝗧𝗶𝗕𝗔𝗡", "⛔ 𝗖̧𝗜𝗞𝗜𝗦̧")
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("578351706144768;564058054983680", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("578351706144768", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(100000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.getResults(100000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("582749752655872;620137442967552", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("582749752655872", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(100000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.getResults(100000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("572854148005888;620137442967552", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("572854148005888", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(100000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.getResults(100000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.clearResults()
+gg.setVisible(false)
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("578351706144768;578351706144768", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.refineNumber("578351706144768", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+gg.getResults(100000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.getResults(100000, nil, nil, nil, nil, nil, nil, nil, nil)
+gg.clearResults()
+gg.alert("✔️ Ꭺɴᴛiʙᴀɴ Ꭺᴋᴛiғ Ꭼᴅiʟᴅi")
+gg.alert("ԃҽƈɾყρƚ Ⴆყ @ԃҽƈɾყρƚαɾαρʂҽʋɱҽɱ")
+HOME = 1
+function HOME()
+MN = gg.choice({
+"⧠ 『 Ꭺɴᴛi Ꭱᴇᴘᴏʀᴛ & Ꮮᴏɢ 』", 
+"'♲ 『 Ꮐ ᴇ ʀ i 』", 
+}, nil, "─────⟦ ◎ İʟʟᴇɢᴀʟ Ꮋᴀᴄᴋ Ͳᴇᴀᴍ ◎ ⟧───── ")
+
+if MN == nil then else
+if MN == 1 then DECRYPTARAPSEVMEM() end
+if MN == 2 then EXIT() end
+end
+PUBGMH = -1
+end
+
+
+function DECRYPTARAPSEVMEM()
+gg.clearResults()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("220676386071773185", gg.TYPE_QWORD)
+gg.getResults(69)
+gg.editAll("220676386036121600", gg.TYPE_QWORD)
+gg.toast(" Ꭰᴀᴛᴀ ϴғғ 📵")
+gg.editAll("220676386071773185", gg.TYPE_QWORD)
+gg.clearResults()
+gg.toast(" Ꭰᴀᴛᴀ ϴɴ 🛰️")
+gg.alert("ԃҽƈɾყρƚ Ⴆყ @ԃҽƈɾყρƚαɾαρʂҽʋɱҽɱ")
+end
+
+function EXIT()
+gggg.alert("ԃҽƈɾყρƚ Ⴆყ @ԃҽƈɾყρƚαɾαρʂҽʋɱҽɱ")
+gg.skipRestoreState()
+gg.setVisible(true)
+os.exit()
+end
+while true do
+  if gg.isVisible(true) then
+    MN = 1
+    gg.setVisible(false)
+  end
+  if MN == 1 then
+    HOME()
+  end
+  end
+ 
+
+
